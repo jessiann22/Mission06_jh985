008effa4a63d4a0b16740fd5210a3963fe31f367
jessiann22 <Jessicaa4755@gmail.com>
2023-02-13 21:00:53 -0700

    Add project files.

---
+++ Mission06_jh985/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission06_jh985.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_jh985.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Podcast()
+        {
+            return View("Podcasts");
+        }
+
+        [HttpGet]
+        public IActionResult MovieEntry()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieEntry(MovieEntry me)
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission06_jh985/Models/MovieEntry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission06_jh985.Models
+{
+    public class MovieEntry
+    {
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        public string Director { get; set; }
+
+        [Required]
+        public string Rating { get; set; }
+        public bool Edited { get; set; }
+        public string Lent { get; set; }
+
+        [MaxLength (25)]
+        public string Notes { get; set; }
+
+    }
+}
